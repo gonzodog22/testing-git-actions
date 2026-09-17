11ab984a2b25dc6da6b277c691954abe8085d939
gonzodog22 <gonzodog22@gmail.com>
2026-09-17 11:46:11 -0600

    new tests

---
+++ public/index.test.js
@@ -0,0 +1,14 @@
+describe('Static HTML Structure', () => {
+    beforeEach(() => {
+        document.body.innnerHTML= 
+            <header>
+                <h1 id="site-title">Welcome</h1>
+            </header>
+    })  
+
+    test('contains the correct main headding', () => {
+        const heading = document.getElementById('sit-title');
+        expect(heading).not.toBeNull();
+        expect(heading.textContent).toBe('Welcome');
+    })
+})
