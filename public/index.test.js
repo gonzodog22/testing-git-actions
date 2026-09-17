b092cf28007c8f9b987d895313c353c4beb3b1cd
gonzodog22 <gonzodog22@gmail.com>
2026-09-17 12:12:12 -0600

    changing header

--- public/index.test.js
+++ public/index.test.js
@@ -1,9 +1,10 @@
 describe('Static HTML Structure', () => {
     beforeEach(() => {
-        document.body.innerHTML= 
+        document.body.innerHTML= `
             <header>
                 <h1 id="site-title">Welcome</h1>
             </header>
+        `
     })  
 
     test('contains the correct main headding', () => {
